7746fd17459b43563f91c417f6694b4b0d4fffa7
chanjong <chanjonggo@gmail.com>
2026-08-23 09:57:26 +0000

    sunday last

--- ros2_ws/src/my_py_pkg/my_py_pkg/robot_news_station.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/robot_news_station.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
- 
+from example_interfaces.msg import String
  
 class RobotNewsStationNode(Node):
     def __init__(self):
         super().__init__("robot_news_station") 
- 
- 
+        #create publisher 
+        #arg 1 = data Type, arg2 = name , arg3 = size of network buffer
+        self.publisher = self.create_publisher(String, "robot_news", 10)
+
+    def publish_news(self):
+        msg = String()
+        msg.data = 
+
 def main(args=None):
     rclpy.init(args=args)
     node = RobotNewsStationNode() 
